880afce8f1d820b5c35079c04daf825c0e6eaaf8
yusuke0324 <take.yusuke@gmail.com>
2017-02-23 12:53:11 +0900

    Complete ~chap03

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -21,7 +21,8 @@ def Mode(hist):
 
     returns: value from Hist
     """
-    return 0
+    f, v = max([(f, v)for v, f in hist.Items()])
+    return v
 
 
 def AllModes(hist):
@@ -31,7 +32,8 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
+
 
 
 def main(script):
